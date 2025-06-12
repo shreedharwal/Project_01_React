5d3041f47b6294961b62600f0862e3dbcd91dd1e
shreya dharwal <107870585+shreedharwal@users.noreply.github.com>
2025-06-12 07:54:51 +0000

    01_Aifinance project start

---
+++ 01_Aifinance/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar.jsx'
+import './App.css'
+
+function App() {
+  
+  return (
+    <main className="bg-black">
+      <Navbar/>
+    </main>
+  )
+}
+
+export default App
